b5f5b3e678f20115f52766ac9df85ef0c6583c51
djpeacher <djpeacher@gmail.com>
2021-01-27 10:34:50 -0600

    1.1.0

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Constants from "expo-constants";
 import {
+  Alert,
   StyleSheet,
   Text,
   View,
@@ -11,6 +12,8 @@ import {
 } from "react-native";
 import * as Haptics from "expo-haptics";
 import { AdMobBanner } from "expo-ads-admob";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { activateKeepAwake, deactivateKeepAwake } from "expo-keep-awake";
 
 import IconButton from "./components/IconButton";
 import * as Dice from "./components/Dice";
@@ -19,9 +22,35 @@ const testID = "ca-app-pub-3940256099942544/2934735716";
 const productionId = "ca-app-pub-3688130715004310/6656983474";
 const adUnitID = Constants.isDevice && !__DEV__ ? productionId : testID;
 
+const getData = async (key) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+};
+
+const setData = async (key, value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
 export default function App() {
   const [formula, setFormula] = useState(defaultFormula);
   const [count, setCount] = useState(-1);
+  const [mode, setMode] = useState(false);
+  const [keepAwake, setkeepAwake] = useState(false);
+
+  useEffect(() => {
+    getData("mode").then((value) => {
+      setMode(value ? value : false);
+    });
+  }, []);
 
   const handleRoll = (dice) => {
     // Update 'count'.
@@ -41,29 +70,87 @@ export default function App() {
     setFormula(defaultFormula);
   };
 
+  const textColor = { color: mode ? "white" : "black" };
+  const iconColor = mode ? "white" : "black";
+
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar />
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: mode ? "black" : "white" }]}
+    >
+      <StatusBar style={mode ? "light" : "dark"} />
+
+      {/* Menu */}
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginHorizontal: 20,
+          marginTop: 5,
+        }}
+      >
+        <IconButton
+          name="lightbulb-outline"
+          onPress={() => {
+            setData("mode", !mode);
+            setMode(!mode);
+          }}
+          color={iconColor}
+          size={30}
+        />
+        <IconButton
+          name={keepAwake ? "flash" : "flash-outline"}
+          onPress={() => {
+            if (!keepAwake) {
+              activateKeepAwake();
+              Alert.alert("Keep Awake", "Your phone will now stay awake!");
+            } else {
+              deactivateKeepAwake();
+            }
+            setkeepAwake(!keepAwake);
+          }}
+          color={iconColor}
+          size={30}
+        />
+      </View>
 
       {/* Formula & Count */}
       <TouchableWithoutFeedback onPress={reset}>
         <View style={styles.display}>
-          <Text style={styles.formula}>{displayFormula(formula)}</Text>
-          {count == -1 && <Text style={styles.ch}>Crit{"\n"}Happens</Text>}
-          {count >= 0 && <Text style={styles.count}>{count}</Text>}
+          <Text style={[styles.formula, textColor]}>
+            {displayFormula(formula)}
+          </Text>
+          {count == -1 && (
+            <>
+              <Text style={[styles.ch, textColor]}>Crit{"\n"}Happens</Text>
+              <TouchableWithoutFeedback
+                onPress={() => Alert.alert("Changelog", changelog)}
+              >
+                <Text
+                  style={{
+                    color: "gray",
+                    paddingVertical: 10,
+                    paddingHorizontal: 50,
+                  }}
+                >
+                  v1.1
+                </Text>
+              </TouchableWithoutFeedback>
+            </>
+          )}
+          {count >= 0 && <Text style={[styles.count, textColor]}>{count}</Text>}
         </View>
       </TouchableWithoutFeedback>
 
       {/* Dice */}
       <View style={styles.diceRow}>
-        <Dice.D4 onPress={handleRoll} />
-        <Dice.D6 onPress={handleRoll} />
-        <Dice.D8 onPress={handleRoll} />
+        <Dice.D4 onPress={handleRoll} color={iconColor} />
+        <Dice.D6 onPress={handleRoll} color={iconColor} />
+        <Dice.D8 onPress={handleRoll} color={iconColor} />
       </View>
       <View style={styles.diceRow}>
-        <Dice.D10 onPress={handleRoll} />
-        <Dice.D12 onPress={handleRoll} />
-        <Dice.D20 onPress={handleRoll} />
+        <Dice.D10 onPress={handleRoll} color={iconColor} />
+        <Dice.D12 onPress={handleRoll} color={iconColor} />
+        <Dice.D20 onPress={handleRoll} color={iconColor} />
       </View>
 
       {/* AD Banner */}
@@ -145,3 +232,15 @@ let defaultFormula = {
   d12: 0,
   d20: 0,
 };
+
+let changelog = `[Unreleased]
+Dice calculator.
+Dice room.
+
+[1.1.0] 2021-01-27
+Added dark/light mode.
+Added keep awake mode.
+Added changelog.
+
+[1.0.0] 2021-01-25
+Added dice roller.`;
--- components/Dice.js
+++ components/Dice.js
@@ -1,26 +1,32 @@
 import React from "react";
 import IconButton from "./IconButton";
 
-export function D4({ onPress }) {
-  return <IconButton name="dice-d4" onPress={() => onPress(4)} />;
+export function D4({ onPress, color }) {
+  return <IconButton name="dice-d4" onPress={() => onPress(4)} color={color} />;
 }
 
-export function D6({ onPress }) {
-  return <IconButton name="dice-d6" onPress={() => onPress(6)} />;
+export function D6({ onPress, color }) {
+  return <IconButton name="dice-d6" onPress={() => onPress(6)} color={color} />;
 }
 
-export function D8({ onPress }) {
-  return <IconButton name="dice-d8" onPress={() => onPress(8)} />;
+export function D8({ onPress, color }) {
+  return <IconButton name="dice-d8" onPress={() => onPress(8)} color={color} />;
 }
 
-export function D10({ onPress }) {
-  return <IconButton name="dice-d10" onPress={() => onPress(10)} />;
+export function D10({ onPress, color }) {
+  return (
+    <IconButton name="dice-d10" onPress={() => onPress(10)} color={color} />
+  );
 }
 
-export function D12({ onPress }) {
-  return <IconButton name="dice-d12" onPress={() => onPress(12)} />;
+export function D12({ onPress, color }) {
+  return (
+    <IconButton name="dice-d12" onPress={() => onPress(12)} color={color} />
+  );
 }
 
-export function D20({ onPress }) {
-  return <IconButton name="dice-d20" onPress={() => onPress(20)} />;
+export function D20({ onPress, color }) {
+  return (
+    <IconButton name="dice-d20" onPress={() => onPress(20)} color={color} />
+  );
 }
